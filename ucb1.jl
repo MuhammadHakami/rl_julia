d589cb4ad8081097a64dcc47dc073e8407b7e098
Muhammad-Hakami <engmuhammadhakami@gmail.com>
2019-11-16 20:14:48 +0300

    ubc1 finished

--- ucb1.jl
+++ ucb1.jl
@@ -1,4 +1,4 @@
-using Plots,Gadfly,GR,PGFPlots,Plotly
+using Plots,LinearAlgebra
 pyplot() # Choose a backend
  # :pyplot
  # :unicodeplots
@@ -25,7 +25,7 @@ function update!(b::Bandit,x::AbstractFloat)
     nothing
 end;
 
-function run_experiment(m1::T, m2::T, m3::T, eps::T, N::Integer, mean::Integer,n::Integer) where {T<:AbstractFloat}
+function run_experiment(m1::T, m2::T, m3::T, eps::T, N::Integer, mean::Integer,n::Integer,ucb1::Bool=false) where {T<:AbstractFloat}
     bandits=[Bandit(m1,mean,n),Bandit(m2,mean,n),Bandit(m3,mean,n)]
     data=Vector{Float64}(undef,N+1)
     for i in collect(1:N+1)
@@ -33,7 +33,7 @@ function run_experiment(m1::T, m2::T, m3::T, eps::T, N::Integer, mean::Integer,n
         if p<eps
             j=rand(collect(1:3))
         else
-            j=argmax([b.mean for b in bandits])
+            j=argmax([b.mean+(sqrt(2*log(exp(1.0),N)/(b.N+1e-3))*ucb1) for b in bandits])
         end
 
             x=pull(bandits[j])
@@ -51,12 +51,12 @@ end
 
 
 function main()
-    c_1=run_experiment(1.0,2.0,3.0,0.1,100000,0,0)
+    ubc1=run_experiment(1.0,2.0,3.0,0.0,100000,0,0,true)
     oiv=run_experiment(1.0,2.0,3.0,0.0,100000,10,1)
-    c_01=run_experiment(1.0,2.0,3.0,0.01,100000,0,0)
-    p=Plots.plot(c_1,label="eps = 0.1",xscale=:log10,legend=:bottomright);
+    c_05=run_experiment(1.0,2.0,3.0,0.05,100000,0,0)
+    p=Plots.plot(ubc1,label="ubc1",xscale=:log10,legend=:bottomright);
     Plots.plot!(p,oiv,label="Optimistic mean=10",xscale=:log10);
-    Plots.plot!(p,c_01,label="eps = 0.01",xscale=:log10);
+    Plots.plot!(p,c_05,label="eps = 0.05",xscale=:log10);
 end
 
 @time main()
