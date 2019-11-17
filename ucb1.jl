de19cc2c20ccf497dddb6f1d67a0d0bb9f641fd0
Muhammad-Hakami <engmuhammadhakami@gmail.com>
2019-11-18 00:40:58 +0300

    finished comparesion of eps greedy oiv ucb1 and bayesian

--- ucb1.jl
+++ ucb1.jl
@@ -1,5 +1,7 @@
-using Plots,LinearAlgebra
-pyplot() # Choose a backend
+using Plots
+module Ucb1
+export run_experiment_ucb1
+# Choose a backend
  # :pyplot
  # :unicodeplots
  # :plotly
@@ -8,25 +10,25 @@ pyplot() # Choose a backend
  # :pgfplots
  # :inspectdr
  # :hdf5
-mutable struct Bandit
+mutable struct Bandit_ucb1
     m::AbstractFloat
     mean::AbstractFloat
     N::Integer
-    Bandit(m,mean=mean,N=N)=new(m,mean,N)
+    Bandit_ucb1(m,mean=mean,N=N)=new(m,mean,N)
 end
 
-function pull(b::Bandit)
+function pull(b::Bandit_ucb1)
     return randn()+b.m
 end
 
-function update!(b::Bandit,x::AbstractFloat)
+function update!(b::Bandit_ucb1,x::AbstractFloat)
     b.N+=1
     b.mean=(1-1.0/b.N)*b.mean+1.0/b.N*x
     nothing
 end;
 
-function run_experiment(m1::T, m2::T, m3::T, eps::T, N::Integer, mean::Integer,n::Integer,ucb1::Bool=false) where {T<:AbstractFloat}
-    bandits=[Bandit(m1,mean,n),Bandit(m2,mean,n),Bandit(m3,mean,n)]
+function run_experiment_ucb1(m1::T, m2::T, m3::T, eps::T, N::Integer, mean::Integer,n::Integer,ucb1::Bool=false) where {T<:AbstractFloat}
+    bandits=[Bandit_ucb1(m1,mean,n),Bandit_ucb1(m2,mean,n),Bandit_ucb1(m3,mean,n)]
     data=Vector{Float64}(undef,N+1)
     for i in collect(1:N+1)
         p=rand()
@@ -42,21 +44,17 @@ function run_experiment(m1::T, m2::T, m3::T, eps::T, N::Integer, mean::Integer,n
         end
     cum_average=cumsum(data)./(collect(1:N+1))
 
-    for b in bandits
-        println(b.mean)
-    end
-    println("----------")
     return cum_average
 end
 
-
-function main()
-    ubc1=run_experiment(1.0,2.0,3.0,0.0,100000,0,0,true)
-    oiv=run_experiment(1.0,2.0,3.0,0.0,100000,10,1)
-    c_05=run_experiment(1.0,2.0,3.0,0.05,100000,0,0)
-    p=Plots.plot(ubc1,label="ubc1",xscale=:log10,legend=:bottomright);
-    Plots.plot!(p,oiv,label="Optimistic mean=10",xscale=:log10);
-    Plots.plot!(p,c_05,label="eps = 0.05",xscale=:log10);
 end
-
-@time main()
+# function main()
+#     ucb1=run_experiment_ucb1(1.0,2.0,3.0,0.0,100000,0,0,true)
+#     oiv=run_experiment_ucb1(1.0,2.0,3.0,0.0,100000,10,1)
+#     c_05=run_experiment_ucb1(1.0,2.0,3.0,0.05,100000,0,0)
+#     p=Plots.plot(ucb1,label="ucb1",xscale=:log10,legend=:bottomright);
+#     Plots.plot!(p,oiv,label="Optimistic mean=10",xscale=:log10);
+#     Plots.plot!(p,c_05,label="eps = 0.05",xscale=:log10);
+# end
+#
+# @time main()
